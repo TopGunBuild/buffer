3ea59dcfc8d74413e8d5b41b6e1e9bf7a580a141
kiborg <easysolpro@gmail.com>
2023-05-03 08:56:12 +0300

    test: add typings

--- src/types.ts
+++ src/types.ts
@@ -1,16 +1,21 @@
+export interface IBufferCore extends Array<number>
+{
+    toString(enc?: string, start?: number, end?: number);
+}
+
 export interface IBuffer {
 
-    from(array: any[]): IBuffer;
-    from(buffer: Uint8Array): IBuffer;
-    from(str: string, encoding?: string): IBuffer;
-    from(value: ArrayBuffer | string | Uint8Array | any[], encodingOrOffset?: number, length?: number): IBuffer;
+    from(array: any[]): IBufferCore;
+    from(buffer: Uint8Array): IBufferCore;
+    from(str: string, encoding?: string): IBufferCore;
+    from(value: ArrayBuffer | string | Uint8Array | any[], encodingOrOffset?: number, length?: number): IBufferCore;
 
-    alloc(size: number, fill?: string | number, encoding?: string): IBuffer;
-    allocUnsafe(length: number): IBuffer;
+    alloc(size: number, fill?: string | number, encoding?: string): IBufferCore;
+    allocUnsafe(length: number): IBufferCore;
 
-    fill(value: number, start?: number, end?: number): IBuffer;
+    fill(value: number, start?: number, end?: number): IBufferCore;
 
-    concat(arr: any[]): IBuffer;
+    concat(arr: any[]): IBufferCore;
 
     toString(encoding?: string, start?: number, end?: number): string;
 }
